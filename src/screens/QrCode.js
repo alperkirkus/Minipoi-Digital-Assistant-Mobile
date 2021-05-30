ded3775ee75c4f4e6e902d234aafff8f781c260b
alperkirkus <alperkirkus@gmail.com>
2021-05-30 22:52:15 +0300

    heroku changes

--- src/screens/QrCode.js
+++ src/screens/QrCode.js
@@ -1,173 +1,96 @@
 import React, { useState, useEffect } from 'react'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import BackButton from '../components/BackButton'
-import TextInput from '../components/TextInput'
-import Button from '../components/Button'
-import { Text, View, StyleSheet, Alert } from 'react-native'
-
-import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import axios from 'axios'
+import Background from '../components/Background'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import BackButton from '../components/BackButton'
+import { StyleSheet, View, Text, Alert } from 'react-native'
+export default function QrCode({ navigation }) {
+  //qr code states
+  const [hasPermission, setHasPermission] = useState(null)
+  const [scanned, setScanned] = useState(false)
 
-export default function Profile({ navigation }) {
-  const [user, setUser] = useState(null)
-  const [oldPass, setOldPass] = useState({ value: '', error: '' })
-  const [newPass, setNewPass] = useState({ value: '', error: '' })
-  const [reNewPass, setReNewPass] = useState({ value: '', error: '' })
-
+  //get permisson
   useEffect(() => {
-    getInfo()
+    ;(async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    })()
   }, [])
 
-  const getInfo = async () => {
-    const token = await AsyncStorage.getItem('@token')
+  //scanner function
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true)
 
+    saveCode(data)
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  }
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>
+  }
+
+  const saveCode = async (code) => {
+    const token = await AsyncStorage.getItem('@token')
     if (token !== null) {
       const config = {
         headers: { Authorization: `Bearer ${token}` },
       }
 
       axios
-        .get('https://mini-back-12.herokuapp.com/api/user/me', config)
+        .get('https://minipoi-back.herokuapp.com/api/user/me', config)
         .then(({ data }) => {
-          setUser(data.user)
-        })
-    }
-  }
-  // change password process
-  const changePassword = async () => {
-    // #TODO  CONTROL Validations
-
-    if (newPass.value !== reNewPass.value) {
-      Alert.alert('Error', 'No matching  new password and re new password')
-    } else if (
-      newPass.value === '' ||
-      reNewPass.value === '' ||
-      oldPass.value === ''
-    ) {
-      Alert.alert('Error', 'Please fill the all fields')
-    } else {
-      const token = await AsyncStorage.getItem('@token')
-      const config = {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-      axios
-        .put(
-          'https://mini-back-12.herokuapp.com/api/user/change-password',
-          {
-            oldPass: oldPass.value,
-            newPass: newPass.value,
-          },
-          config
-        )
-        .then(async (res) => {
-          // await AsyncStorage.setItem("@token",res.data.token)
-
-          setNewPass({ value: '', error: '' })
-          setReNewPass({ value: '', error: '' })
-          setOldPass({ value: '', error: '' })
-          Alert.alert('Success', 'Your password has been changed successfuly', [
-            {
-              text: 'OK',
-              onPress: () => {
-                navigation.reset({
-                  index: 0,
-                  routes: [{ name: 'Dashboard' }],
-                })
-              },
-            },
-          ])
-        })
-        .catch((err) => {
-          Alert.alert('Error', 'Something went wrong')
+          let postData = {
+            code: code,
+            userId: data.user.id,
+          }
+          axios
+            .post(
+              'https://minipoi-back.herokuapp.com/api/code/add-book',
+              postData
+            )
+            .then(({ data }) => {
+              if (data.msg === 'success') {
+                Alert.alert('Success', 'Book has been added', [
+                  {
+                    text: 'OK',
+                    onPress: () => {
+                      navigation.goBack()
+                    },
+                  },
+                ])
+              } else {
+                Alert.alert('Warning', 'Code has already taken', [
+                  {
+                    text: 'OK',
+                    onPress: () => {
+                      navigation.goBack()
+                    },
+                  },
+                ])
+              }
+            })
         })
     }
   }
 
   return (
-    <Background navigation={navigation}>
+    <View style={styles.background}>
       <BackButton goBack={navigation.goBack} />
-      <Logo />
-      <Header>Profile</Header>
-
-      <View>
-        <Text style={styles.text}>Username : {user?.username}</Text>
-        <Text style={styles.text}>E-mail : {user?.email}</Text>
-      </View>
-
-      <Text style={styles.title}>Change Password</Text>
-
-      <View style={styles.line}></View>
-
-      <TextInput
-        label="Old Password"
-        returnKeyType="next"
-        value={oldPass.value}
-        onChangeText={(text) => setOldPass({ value: text, error: '' })}
-        error={!!oldPass.error}
-        errorText={oldPass.error}
-        autoCapitalize="none"
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
       />
-
-      <TextInput
-        label="New Password"
-        returnKeyType="next"
-        value={newPass.value}
-        onChangeText={(text) => setNewPass({ value: text, error: '' })}
-        error={!!newPass.error}
-        errorText={newPass.error}
-        autoCapitalize="none"
-      />
-      <TextInput
-        label="Re New Password"
-        returnKeyType="next"
-        value={reNewPass.value}
-        onChangeText={(text) => setReNewPass({ value: text, error: '' })}
-        error={!!reNewPass.error}
-        errorText={reNewPass.error}
-        autoCapitalize="none"
-      />
-
-      <Button
-        style={styles.button}
-        labelStyle={{ color: 'white' }}
-        color="#581845"
-        mode="contained"
-        onPress={() => changePassword()}
-      >
-        Save
-      </Button>
-    </Background>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 15,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '48%',
-    textAlign: 'center',
-  },
-  title: {
-    color: 'blue',
-    fontSize: 20,
-    marginTop: 10,
-  },
-  line: {
-    marginTop: 5,
-    marginBottom: 5,
-    height: 1,
+  background: {
+    flex: 1,
     width: '100%',
-    backgroundColor: 'blue',
   },
 })
